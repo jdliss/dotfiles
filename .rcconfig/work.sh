d96756c23b0de4c7f98302799178cba60ce3953b
Jonathan <jonacom@lissismore.com>
2019-09-26 10:32:13 -0600

    add more work commands

--- .rcconfig/work.sh
+++ .rcconfig/work.sh
@@ -1,2 +1,5 @@
 alias ssh-staging="ssh -i ~/.ssh/moto-cluster.pem $STAGING_ADDR"
 alias ssh-dev="ssh -i ~/.ssh/moto-cluster.pem $DEV_ADDR"
+alias moto-solr-console='echo "Connect to solr on http://localhost:9000" && ssh -i ~/.ssh/moto-cluster.pem moto@$SOLR_DEV_HOST -L 9000:localhost:8983'
+alias moto-solr-ssh='ssh -i ~/.ssh/moto-cluster.pem moto@$SOLR_DEV_HOST'
+
