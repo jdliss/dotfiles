67ded756bbd37f5b977d07deecfdbbfa4a1ff206
Jonathan <jonacom@lissismore.com>
2019-11-22 14:50:29 -0700

    add another ssh command

--- .rcconfig/work.sh
+++ .rcconfig/work.sh
@@ -3,3 +3,7 @@ alias ssh-solr-dev='ssh -i ~/.ssh/moto-cluster.pem moto@$SOLR_DEV_HOST'
 alias ssh-staging="ssh -i ~/.ssh/moto-cluster.pem $STAGING_ADDR"
 alias ssh-solr-dev-console='echo "Connect to solr on http://localhost:9000" && ssh -i ~/.ssh/moto-cluster.pem moto@$SOLR_DEV_HOST -L 9000:localhost:8983'
 
+ssh-web-prod-b() {
+  cd $MOTO_OPS_HOME/terraform/prod-b && script/ssh-web $1
+}
+
